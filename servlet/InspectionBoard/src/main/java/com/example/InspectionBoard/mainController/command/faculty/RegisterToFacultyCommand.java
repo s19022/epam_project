a7b329e1dc4ac0ef1b36ed271290594588372eb4
s19022 <s19022@gmail.com>
2021-06-02 16:01:56 +0300

    added FacultyRegistrationStatus

--- servlet/InspectionBoard/src/main/java/com/example/InspectionBoard/mainController/command/faculty/RegisterToFacultyCommand.java
+++ servlet/InspectionBoard/src/main/java/com/example/InspectionBoard/mainController/command/faculty/RegisterToFacultyCommand.java
@@ -5,10 +5,13 @@ import com.example.InspectionBoard.exceptions.CannotRegisterToFacultyException;
 import com.example.InspectionBoard.exceptions.NoSuchAccountException;
 import com.example.InspectionBoard.exceptions.NoSuchFacultyException;
 import com.example.InspectionBoard.mainController.command.Command;
+import com.example.InspectionBoard.model.enums.FacultyRegistrationStatus;
 import com.example.InspectionBoard.model.enums.RequestType;
 import com.example.InspectionBoard.model.service.FacultyRegistrationService;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.example.InspectionBoard.model.enums.FacultyRegistrationStatus.*;
 
 public class RegisterToFacultyCommand implements Command {
     @Override
@@ -17,10 +20,20 @@ public class RegisterToFacultyCommand implements Command {
         String facultyName = request.getParameter("facultyName");
         try {
             new FacultyRegistrationService().register(enrolleeLogin, facultyName);
-            request.setAttribute("facultyRegistrationStatus", "Success!");
-        } catch (NoSuchAccountException | NoSuchFacultyException | CannotRegisterToFacultyException | AlreadyRegisteredException e) {
-            request.setAttribute("facultyRegistrationStatus", e);
+            setFacultyRegistrationStatus(request, SUCCESSFULLY);
+        } catch (NoSuchAccountException e) {
+            setFacultyRegistrationStatus(request, NO_SUCH_ACCOUNT);
+        } catch (NoSuchFacultyException e) {
+            setFacultyRegistrationStatus(request, NO_SUCH_FACULTY);
+        } catch (CannotRegisterToFacultyException e) {
+            setFacultyRegistrationStatus(request, CANNOT_REGISTER);
+        } catch (AlreadyRegisteredException e) {
+            setFacultyRegistrationStatus(request, ALREADY_REGISTERED);
         }
         return "/faculty/info.jsp";
+    }
+
+    private static void setFacultyRegistrationStatus(HttpServletRequest request, FacultyRegistrationStatus status){
+        request.setAttribute("facultyRegistrationStatus", status);
     }
 }
---
+++ servlet/InspectionBoard/src/main/java/com/example/InspectionBoard/model/enums/FacultyRegistrationStatus.java
@@ -0,0 +1,9 @@
+package com.example.InspectionBoard.model.enums;
+
+public enum FacultyRegistrationStatus {
+    SUCCESSFULLY,
+    NO_SUCH_ACCOUNT,
+    NO_SUCH_FACULTY,
+    CANNOT_REGISTER,
+    ALREADY_REGISTERED
+}
