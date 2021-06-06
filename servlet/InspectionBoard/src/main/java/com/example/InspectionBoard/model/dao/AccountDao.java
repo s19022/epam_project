42ce3cbe74ada063da4c2fde7b4514a49ae1cc8f
s19022 <s19022@gmail.com>
2021-06-06 21:40:21 +0300

    deleted unnecessary throws declarations

--- servlet/InspectionBoard/src/main/java/com/example/InspectionBoard/model/dao/AccountDao.java
+++ servlet/InspectionBoard/src/main/java/com/example/InspectionBoard/model/dao/AccountDao.java
@@ -1,7 +1,5 @@
 package com.example.InspectionBoard.model.dao;
 
-import com.example.InspectionBoard.exceptions.AccountIsBlockedException;
-import com.example.InspectionBoard.exceptions.WrongLoginPasswordException;
 import com.example.InspectionBoard.model.dto.SaveEnrolleeDto;
 import com.example.InspectionBoard.model.dto.db.DbAccountDto;
 
@@ -9,8 +7,7 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 public interface AccountDao extends GenericDao<DbAccountDto> {
-    Optional<DbAccountDto> findByLoginAndPassword(String login, String password)
-            throws WrongLoginPasswordException, AccountIsBlockedException, SQLException;
+    Optional<DbAccountDto> findByLoginAndPassword(String login, String password) throws  SQLException;
     void blockEnrollee(String login) throws SQLException;
     void unblockEnrollee(String login) throws SQLException;
     void insertEnrollee(SaveEnrolleeDto enrollee, int id) throws SQLException;
